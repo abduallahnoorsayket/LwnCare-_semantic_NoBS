7b22cbb99439fd767b5812ae5733ef1bb7f66bdb
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2023-04-18 15:52:09 +0600

    gallery page added

--- js/script.js
+++ js/script.js
@@ -6,3 +6,36 @@ function classToggle() {
 document
   .querySelector(".Navbar__Link-toggle")
   .addEventListener("click", classToggle);
+
+(function () {
+  "use strict";
+
+  function carousels() {
+    $(".owl-carousel1").owlCarousel({
+      loop: true,
+      center: true,
+      margin: 0,
+      responsiveClass: true,
+      nav: false,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: false,
+        },
+        1000: {
+          items: 3,
+          nav: true,
+        },
+      },
+    });
+  }
+
+  (function ($) {
+    carousels();
+  })(jQuery);
+})();
